144e8ab392f9eab37f8480020fa64a4cb9450cb3
Sean Bruce <seanbruce@hotmail.com>
2020-04-15 14:57:02 -0700

    4/15 docs

--- 002- Create Orders/450orders/routes/index.js
+++ 002- Create Orders/450orders/routes/index.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require("mongoose");
+
+const Orders = require("../Orders");
+
+const dbURI = "mongodb+srv://bcuser:<password>@cluster0-8ybiz.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.set('useFindAndModify', false);
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
+
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile('index.html');
