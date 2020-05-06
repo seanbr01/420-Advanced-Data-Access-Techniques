c3afdf21d8539f5e42d442605eb46d1809385f00
Sean Bruce <seanbruce@hotmail.com>
2020-05-06 15:13:51 -0700

    5/6 docs

---
+++ ProductsSol/Products/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Products.Models
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public int Priority { get; set; }
+        public string Subject { get; set; }
+        public string Details { get; set; }
+    }
+}
